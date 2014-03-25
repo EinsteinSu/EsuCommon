d8c96de014fe293acf15274b327ec0fa738bf735
Einstein Su <einstein_supeng@hotmail.com>
2014-03-25 17:34:44 +0800

    Initial commit

---
+++ Supeng.Sports.Common/Timers/Extensions/EsuDescTimerWithButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supeng.Common.Entities;
+
+namespace Supeng.Sports.Common.Timers.Extensions
+{
+  public class EsuDescTimerWithButton : EsuInfoBase
+  {
+  }
+}
---
+++ Supeng.Wpf.Common/Entities/EsuButtonBase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows;
+using DevExpress.Xpf.Bars;
+using Supeng.Common.Entities;
+using Supeng.Common.IOs;
+
+namespace Supeng.Wpf.Common.Entities
+{
+  public class EsuButtonBase : EsuInfoBase
+  {
+    private string text;
+    private bool enable;
+    private Thickness thickness = new Thickness(0);
+    private string imageUrl;
+    private readonly DelegateCommand command;
+
+    public EsuButtonBase()
+    {
+
+    }
+
+    public EsuButtonBase(Action action)
+    {
+      command = new DelegateCommand(action, () => true);
+    }
+
+    public EsuButtonBase(string text, int left, Action action)
+      : this(action)
+    {
+      this.text = text;
+      thickness = new Thickness(left, 0, 0, 0);
+    }
+
+    #region properties
+    public string Text
+    {
+      get { return text; }
+      set
+      {
+        if (value == text) return;
+        text = value;
+        NotifyOfPropertyChange(() => Text);
+      }
+    }
+
+    public bool Enable
+    {
+      get { return enable; }
+      set
+      {
+        if (value.Equals(enable)) return;
+        enable = value;
+        NotifyOfPropertyChange(() => Enable);
+      }
+    }
+
+    public Thickness Thickness
+    {
+      get { return thickness; }
+      set
+      {
+        if (value.Equals(thickness)) return;
+        thickness = value;
+        NotifyOfPropertyChange(() => Thickness);
+      }
+    }
+
+    public string ImageUrl
+    {
+      get
+      {
+        if (string.IsNullOrEmpty(imageUrl))
+          return string.Format("{0}Buttons\\{1}.png", DirectoryHelper.ImageDirectory, Text);
+        return imageUrl;
+      }
+      set
+      {
+        if (value == imageUrl) return;
+        imageUrl = value;
+        NotifyOfPropertyChange(() => ImageUrl);
+      }
+    }
+
+    public DelegateCommand Command
+    {
+      get { return command; }
+    }
+    #endregion
+  }
+}
