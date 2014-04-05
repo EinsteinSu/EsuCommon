a65842a4e11df31ad8bae8f787ac610549a86564
EinsteinSu <Einstein.Su@software.dell.com>
2014-04-05 23:24:11 +0800

    add type convert class and unit test

---
+++ Supeng.Common.Tests/DataConvertTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Supeng.Common.Types;
+
+namespace Supeng.Common.Tests
+{
+  [TestFixture]
+  public class DataConvertTests
+  {
+    [Test]
+    public void TestDataConvert()
+    {
+      string data = "1";
+      int i = data.ConvertData<int>();
+      Assert.AreEqual(1, i);
+
+      decimal d = data.ConvertData<decimal>();
+      Assert.AreEqual(1, d);
+
+      //DateTime time = DateTime.Now;
+      //data = time.ToString(CultureInfo.InvariantCulture);
+
+      //var result = data.ConvertData<DateTime>();
+      //Assert.AreEqual(time, result);
+    }
+  }
+}
---
+++ Supeng.Common/Types/TypeConvertHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Supeng.Common.Types
+{
+  public static class TypeConvertHelper
+  {
+    public static T ConvertData<T>(this string data, T defaultValue = default (T))
+    {
+      if (string.IsNullOrEmpty(data))
+        return defaultValue;
+      var memberInfo = typeof(T).GetMethod("TryParse", new[] { data.GetType(), typeof(T).MakeByRefType() });
+      if (memberInfo != null)
+      {
+        bool b;
+        object[] parameters = { data, null };
+        bool.TryParse(memberInfo.Invoke(null, parameters).ToString(), out b);
+        if (b && parameters.Count() > 1)
+          return (T)parameters[1];
+      }
+      return defaultValue;
+    }
+
+    public static T EnumConvert<T>(this string data) where T : struct
+    {
+      T e;
+      Enum.TryParse(data, out e);
+      return e;
+    }
+
+    public static DateTime ConvertToDateTime(this string data)
+    {
+      return data.ConvertData(new DateTime(1900, 1, 1));
+    }
+  }
+}
