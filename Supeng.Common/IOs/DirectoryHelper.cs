57143b3b32db42cc5c10009ea3b6fc41f6b31dd3
EinsteinSu <einstein_supeng@hotmail.com>
2015-02-25 17:34:59 +0800

    DirectoryHelper format

--- Supeng.Common/IOs/DirectoryHelper.cs
+++ Supeng.Common/IOs/DirectoryHelper.cs
@@ -4,71 +4,71 @@ using System.Reflection;
 
 namespace Supeng.Common.IOs
 {
-    public static class DirectoryHelper
+  public static class DirectoryHelper
+  {
+    public static string DataDirectory
     {
-        public static string DataDirectory
-        {
-            get
-            {
-                string dataDirectory = string.Format("{0}\\Data\\", Environment.CurrentDirectory);
-                if (!Directory.Exists(dataDirectory))
-                    Directory.CreateDirectory(dataDirectory);
-                return dataDirectory;
-            }
-        }
-
-        public static string ImageDirectory
-        {
-            get
-            {
-                string dataDirectory = string.Format("{0}\\Images\\", Environment.CurrentDirectory);
-                if (!Directory.Exists(dataDirectory))
-                    Directory.CreateDirectory(dataDirectory);
-                return dataDirectory;
-            }
-        }
-
-        public static string TemplateDirectory
-        {
-            get
-            {
-                string dataDirectory = string.Format("{0}\\Templates\\", Environment.CurrentDirectory);
-                if (!Directory.Exists(dataDirectory))
-                    Directory.CreateDirectory(dataDirectory);
-                return dataDirectory;
-            }
-        }
-
-        public static string TempDirectory
-        {
-            get
-            {
-                string dataDirectory = string.Format("{0}\\Temps\\", Environment.CurrentDirectory);
-                if (!Directory.Exists(dataDirectory))
-                    Directory.CreateDirectory(dataDirectory);
-                return dataDirectory;
-            }
-        }
-
-        /// <summary>
-        /// get a temp files with guid name
-        /// </summary>
-        /// <param name="extendName">extend name(.doc)</param>
-        /// <returns></returns>
-        public static string GetTempFile(string extendName)
-        {
-            return string.Format("{0}{1}{2}", TempDirectory, Guid.NewGuid(), extendName);
-        }
-
-        public static string LogDirectory
-        {
-            get
-            {
-                string dataDirectory = string.Format("{0}\\Logs\\", Environment.CurrentDirectory);
-                if (!Directory.Exists(dataDirectory))
-                    Directory.CreateDirectory(dataDirectory);
-                return dataDirectory;
-            }
-        }
+      get
+      {
+        string dataDirectory = string.Format("{0}\\Data\\", Environment.CurrentDirectory);
+        if (!Directory.Exists(dataDirectory))
+          Directory.CreateDirectory(dataDirectory);
+        return dataDirectory;
+      }
     }
+
+    public static string ImageDirectory
+    {
+      get
+      {
+        string dataDirectory = string.Format("{0}\\Images\\", Environment.CurrentDirectory);
+        if (!Directory.Exists(dataDirectory))
+          Directory.CreateDirectory(dataDirectory);
+        return dataDirectory;
+      }
+    }
+
+    public static string TemplateDirectory
+    {
+      get
+      {
+        string dataDirectory = string.Format("{0}\\Templates\\", Environment.CurrentDirectory);
+        if (!Directory.Exists(dataDirectory))
+          Directory.CreateDirectory(dataDirectory);
+        return dataDirectory;
+      }
+    }
+
+    public static string TempDirectory
+    {
+      get
+      {
+        string dataDirectory = string.Format("{0}\\Temps\\", Environment.CurrentDirectory);
+        if (!Directory.Exists(dataDirectory))
+          Directory.CreateDirectory(dataDirectory);
+        return dataDirectory;
+      }
+    }
+
+    /// <summary>
+    /// get a temp files with guid name
+    /// </summary>
+    /// <param name="extendName">extend name(.doc)</param>
+    /// <returns></returns>
+    public static string GetTempFile(string extendName)
+    {
+      return string.Format("{0}{1}{2}", TempDirectory, Guid.NewGuid(), extendName);
+    }
+
+    public static string LogDirectory
+    {
+      get
+      {
+        string dataDirectory = string.Format("{0}\\Logs\\", Environment.CurrentDirectory);
+        if (!Directory.Exists(dataDirectory))
+          Directory.CreateDirectory(dataDirectory);
+        return dataDirectory;
+      }
+    }
+  }
 }
