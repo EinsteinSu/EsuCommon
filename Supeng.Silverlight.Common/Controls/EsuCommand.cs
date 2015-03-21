0fd135fbe8a70c54b80fcebdd611fcd7791ce20c
EinsteinSu <einstein_supeng@hotmail.com>
2015-03-21 11:56:44 +0800

    add a EsuCommand in sl common and common project

---
+++ Supeng.Silverlight.Common/Controls/EsuCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Supeng.Silverlight.Common.Controls
+{
+  public class EsuCommand : ICommand
+  {
+    private readonly Action execute;
+    private readonly Func<bool> canExecute;
+
+    public EsuCommand(Action execute, Func<bool> canExecute = null)
+    {
+      this.execute = execute;
+      this.canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+      return canExecute == null || canExecute();
+    }
+
+    public void Execute(object parameter)
+    {
+      execute();
+    }
+
+    public event EventHandler CanExecuteChanged;
+  }
+
+  public class EsuCommandWithParameter<T> : ICommand
+  {
+    private readonly Action<T> execute;
+    private readonly Func<bool> canExecute;
+
+    public EsuCommandWithParameter(Action<T> execute, Func<bool> canExecute = null)
+    {
+      this.execute = execute;
+      this.canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+      return canExecute == null || canExecute();
+    }
+
+    public void Execute(object parameter)
+    {
+      var data = (T)parameter;
+      execute(data);
+    }
+
+    public event EventHandler CanExecuteChanged;
+  }
+}
