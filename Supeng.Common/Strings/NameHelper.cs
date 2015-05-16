c8b4cfb0b18e4b60ba3fb4102bf0d083bc946063
EinsteinSu <einstein_supeng@hotmail.com>
2015-05-16 17:15:30 +0800

    add a class of NameHelper

---
+++ Supeng.Common/Strings/NameHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supeng.Common.Strings
+{
+  public static class NameHelper
+  {
+    readonly static IList<string> CompoundNames = new[] { "欧阳", "慕容" };
+
+    public static string GetFirstName(this string name)
+    {
+
+    }
+  }
+}
