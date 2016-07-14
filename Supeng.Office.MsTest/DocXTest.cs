b053dfa2f629e7e40a0179195c5855e2f117a156
einstein su <einstein_supeng@hotmail.com>
2016-07-14 15:11:18 +0800

    word operation

---
+++ Supeng.Office.MsTest/DocXTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Novacode;
+
+namespace Supeng.Office.MsTest
+{
+    [TestClass]
+    public class DocXTest
+    {
+        private DocXWordOperation docx; // = new DocXWordOperation("Test");
+        private string target;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var template = Path.Combine(Directory.GetCurrentDirectory(), "Data", "template.docx");
+            target = Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid() + ".docx");
+            File.Copy(template, target);
+            docx = new DocXWordOperation(target);
+        }
+
+        [TestMethod]
+        public void ReplaceText()
+        {
+            var list = new List<WordReplaceInfo>();
+            list.Add(new WordReplaceInfo("Test", "Test1"));
+            docx.Replace(list);
+            docx.Dispose();
+        }
+
+        [TestMethod]
+        public void InsertText()
+        {
+            docx.InsertText("bookmark1", "This is bookmark1");
+            docx.Dispose();
+        }
+
+        [TestMethod]
+        public void InsertTable()
+        {
+            docx.InsertTable("bookmark2", new TableTest());
+            docx.Dispose();
+        }
+
+        [TestCleanup]
+        public void Clean()
+        {
+            if (!File.Exists(target))
+            {
+                File.Delete(target);
+            }
+        }
+    }
+
+    public class TableTest : ITableInsert
+    {
+        public int RowCount
+        {
+            get { return 5; }
+        }
+
+        public int ColumnCount
+        {
+            get { return 5; }
+        }
+
+        public void FillData(Table table)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    table.Rows[i].Cells[j].Paragraphs[0].Append(string.Format("Rows:{0} Columns:{1}", i, j));
+                }
+            }
+
+        }
+
+        public void InsertHead(Table table)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                table.Rows[0].Cells[i].Paragraphs[0].Append("Column Head " + i);
+            }
+        }
+    }
+}
---
+++ Supeng.Office/DocXWordOperation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Novacode;
+
+namespace Supeng.Office
+{
+    public class DocXWordOperation : IWordOperation
+    {
+        private readonly DocX _doc;
+
+        public DocXWordOperation(string fileName)
+        {
+            _doc = !File.Exists(fileName) ? DocX.Create(fileName) : DocX.Load(fileName);
+        }
+
+        public void Dispose()
+        {
+            _doc.Save();
+            _doc.Dispose();
+        }
+
+        public DocX Doc
+        {
+            get { return _doc; }
+        }
+
+        public void InsertText(string bookMark, string text)
+        {
+            var item = _doc.Bookmarks[bookMark];
+            if (item != null)
+            {
+                item.Paragraph.InsertText(text);
+            }
+        }
+
+        public void InsertTable(string bookMark, ITableInsert tableInsert,
+            TableDesign tableDesign = TableDesign.TableGrid)
+        {
+            var item = _doc.Bookmarks[bookMark];
+            if (item != null)
+            {
+                var table = _doc.AddTable(tableInsert.RowCount, tableInsert.ColumnCount);
+                table.Design = tableDesign;
+                tableInsert.InsertHead(table);
+                tableInsert.FillData(table);
+                item.Paragraph.InsertTableAfterSelf(table);
+            }
+        }
+
+        public void Replace(IList<WordReplaceInfo> replaces)
+        {
+            foreach (var item in replaces)
+            {
+                _doc.ReplaceText(item.Oldsring, item.Newstring);
+            }
+        }
+    }
+}
